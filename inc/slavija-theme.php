de9f75b521020bf908f4094bbf45282b787d1427
Luka Orlandić <orlandolukic96@gmail.com>
2023-01-15 16:24:41 +0100

    Added support for keywords on specific pages & added Google Analytics

--- inc/slavija-theme.php
+++ inc/slavija-theme.php
@@ -10,6 +10,7 @@ class SlavijaTheme {
         add_action("slavija_after_header", "SlavijaTheme::after_header");
         add_action("wp_enqueue_scripts", "SlavijaTheme::enqueue_scripts", 5);
         add_action("wp_head", "SlavijaTheme::initiate_keywords", 0);
+        add_action("wp_head", "SlavijaTheme::google_analytics");
         add_action("wp_footer", "SlavijaTheme::schema_org");
     }
 
@@ -27,13 +28,66 @@ class SlavijaTheme {
         wp_enqueue_style("fontawesome", get_template_directory_uri() . "/styles/fa-all.min.css");
     }
 
+    public static function google_analytics() {
+        ?>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W7M9JZZGNQ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-W7M9JZZGNQ');
+        </script>
+        <?php
+    }
+
     public static function initiate_keywords() {
-        global $post;
-        $keywords = __("knjigovodja, knjigovođa, knjigovodstvo, racunovodstvo, računovodstvo, e fakture, e-fakture, zavrsni racun, završni račun, zavrsni racun izrada, završni račun izrada, poresko savetovanje, obračun zarada, obracun zarada, poreske prijave, finansijski izveštaji, finansijski izvestaji, otvaranje firme", "slavija");
+        $keywords = SlavijaTheme::get_keywords();
         ?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keywords" content="<?= $keywords ?>">
         <?php
+    }
+
+    private static function get_keywords() {
+
+        // TODO: Add support for other languages
+
+        global $post;
+        $kw = "";
+        switch( $post->ID ) {
+        // Homepage
+        case 7:         // SR
+        // O nama
+        case 64:        // SR
+        // Usluge, paket usluga
+        case 70:        // SR
+        default:
+            $kw = __("slavija knjigovodstvo, slavija, knjigovodja, knjigovođa, knjigovodstvo, racunovodstvo, računovodstvo, e fakture, e-fakture, zavrsni racun, završni račun, zavrsni racun izrada, završni račun izrada, poresko savetovanje, obračun zarada, obracun zarada, poreske prijave, finansijski izveštaji, finansijski izvestaji, otvaranje firme", "slavija");
+            break;
+
+        // Ovlasceni racunovodja
+        case 5575:      // SR
+            $kw = __("knjigovodstvo, ovlasceni racunovodja, ovlašćeni računovođa, ovlasceni racunovodja beograd, ovlašćeni računovođa beograd, knjigovodstvo, racunovodstvo, računovodstvo, e fakture, e-fakture", "slavija");
+            break;
+
+        // On-line racunovodstvo
+        case 93:        // SR
+            $kw = __("knjigovodstvo, racunovodstvo online, online racunovodstvo, računovodstvo online, online računovodstvo, online vodjenje knjiga, online knjigovodstvo, online knjigovodstvo beograd, e fakture", "slavija");
+            break;
+
+        // Izrada zavrsnog racuna
+        case 97:        // SR
+            $kw = __("knjigovodstvo, izrada zavrsnog racuna, izrada zavrsnog racuna beograd, izrada završnog računa, izrada završnog računa beograd, zavrsni racun iskustvo, završni račun iskustvo", "slavija");
+            break;
+
+        // Poresko i poslovno savetovanje
+        case 91:        // SR
+            $kw = __("poresko savetovanje, poresko savetovanje beograd, poslovno savetovanje, poslovno savetovanje beograd, knjigovodstvo konsultacije, knjigovodstvo iskustvo", "slavija");
+            break;
+        }
+
+        return $kw;
     }
 
     public static function schema_org() {
